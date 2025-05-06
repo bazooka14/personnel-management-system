b5c8fc71ae3f9dd464501bc3cbfb68ea4e04a939
Magomedpasha Sultanov <sultanov.mps@mail.ru>
2025-05-06 20:26:42 +0300

    refactor: move models to model package

--- EmptyPerson.java
+++
@@ -1,5 +0,0 @@
-public class EmptyPerson extends Person {
-    public EmptyPerson(String name, int age) {
-        super(name, age);
-    }
-}
---
+++ model/EmptyPerson.java
@@ -0,0 +1,7 @@
+package model;
+
+public class EmptyPerson extends Person {
+    public EmptyPerson() {
+        super("empty", 0);
+    }
+}
