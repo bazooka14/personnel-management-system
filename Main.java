ad39eceb4bf7e776caf7e60907d605fb339fa8e6
Magomedpasha Sultanov <sultanov.mps@mail.ru>
2025-05-09 10:45:02 +0300

    Add test cases in Main class

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import init.DataInitializer;
+import manager.DataManager;
 import model.Person;
 import model.Staff;
+import printer.ConsoleDataPrinter;
+import printer.DataPrinter;
+import repository.ArrayBasedPersonRepository;
+import repository.PersonRepository;
 
 public class Main {
     public static void main(String[] args) {
-        Person staff = new Staff("Паша", 24, 100000);
-        System.out.println(staff);
+        PersonRepository personRepository = new ArrayBasedPersonRepository();
+        DataPrinter<PersonRepository> dataPrinter = new ConsoleDataPrinter();
+
+        DataManager dataManager = new DataManager(personRepository, dataPrinter);
+        DataInitializer dataInitializer = new DataInitializer(dataManager);
+
+        dataInitializer.populate(30);
+        dataManager.displayAllPerson();
+
+        System.out.println();
+        System.out.println(dataManager.findById(4));
+        System.out.println(dataManager.findById(33));
     }
 }
